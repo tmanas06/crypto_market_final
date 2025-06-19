92728ec308759073613d5700724752333bfa1ad5
tmanas06 <2210030003@klh.edu.in>
2025-06-19 20:08:58 +0530

    err

--- src/components/CryptoChart.tsx
+++ src/components/CryptoChart.tsx
@@ -1,5 +1,3 @@
-import TradingViewWidget from 'react-tradingview-widget';
-
 const CryptoChart = () => {
   return (
     <div className="glass-card p-6 rounded-lg mb-8 animate-fade-in">
@@ -7,19 +5,11 @@ const CryptoChart = () => {
         <h2 className="text-xl font-semibold">Bitcoin Price</h2>
       </div>
       <div className="h-[400px] w-full">
-        <TradingViewWidget
-          symbol="BINANCE:BTCUSDT"
-          theme="Dark"
-          locale="en"
-          autosize
-          hide_side_toolbar={false}
-          allow_symbol_change={true}
-          interval="D"
-          toolbar_bg="#141413"
-          enable_publishing={false}
-          hide_top_toolbar={false}
-          save_image={false}
-          container_id="tradingview_chart"
+        <iframe
+          src="https://s.tradingview.com/widgetembed/?frameElementId=tradingview_12345&symbol=BINANCE%3ABTCUSDT&interval=D&symboledit=1&saveimage=1&toolbarbg=f1f3f6&studies=%5B%5D&theme=dark&style=1&timezone=Etc%2FUTC&withdateranges=true&hide_top_toolbar=0&hide_side_toolbar=0&allow_symbol_change=1&show_popup_button=1&popup_width=1000&popup_height=650"
+          className="w-full h-full rounded-lg"
+          title="TradingView Widget"
+          id="tradingview_12345"
         />
       </div>
     </div>
